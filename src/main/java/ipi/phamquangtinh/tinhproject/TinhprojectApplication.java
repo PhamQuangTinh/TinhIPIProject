8065933cc9d87938cc29b6844a0769c316470d28
tinh.pham <tinh.pham@ipicorp.co>
2021-03-06 16:07:20 +0700

    first commit

---
+++ src/main/java/ipi/phamquangtinh/tinhproject/TinhprojectApplication.java
@@ -0,0 +1,13 @@
+package ipi.phamquangtinh.tinhproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TinhprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TinhprojectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ipi/phamquangtinh/tinhproject/TinhprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package ipi.phamquangtinh.tinhproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TinhprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
